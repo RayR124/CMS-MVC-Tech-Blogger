984b329584c7c7d66684f6977bfd4456ae485362
RayR124 <RealtorRayR@Gmail.com>
2023-02-13 19:09:53 -0800

    fixed server.js so it loads the main.handlebars now

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const controllers = require('./controllers');
 const sequelize = require('./config/connection');
+const handlebars = require('handlebars');
 const { engine } = require('express-handlebars');
 
 const app = express();
@@ -13,8 +14,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(controllers);
 app.use(express.static('public'));
 
-app.engine('handlebars', engine());
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
